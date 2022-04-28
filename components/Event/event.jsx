91c69737246d3644f27a262b4b37b30b6d18d1c0
Matthew Lim <personal.matthewjlim@gmail.com>
2022-04-28 10:30:30 -0700

    update event

--- components/Event/event.jsx
+++ components/Event/event.jsx
@@ -1,81 +1,54 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import { Box } from '@mui/material';
-import Paper from '@mui/material/Paper';
+import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
-import ButtonBase from '@mui/material/ButtonBase';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
+function FeaturedPost(props) {
+  const { post } = props;
 
-function ComplexGrid() {
   return (
-    <Paper
-      sx={{
-        p: 2,
-        margin: 'auto',
-        maxWidth: 500,
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      }}
-    >
-      <Grid container spacing={2}>
-        <Grid item>
-          <ButtonBase sx={{ width: 128, height: 128 }}>
-            <Img alt="complex" src="https://media-exp1.licdn.com/dms/image/C5603AQH0gseaGH4yQQ/profile-displayphoto-shrink_800_800/0/1642016037479?e=1656547200&v=beta&t=OHCL_-a6-5ucXzy40pWahqFLqXjWBNKZyZqyew9XpbI" />
-          </ButtonBase>
-        </Grid>
-        <Grid item xs={12} sm container>
-          <Grid item xs container direction="column" spacing={2}>
-            <Grid item xs>
-              <Typography gutterBottom variant="subtitle1" component="div">
-                Standard license
-              </Typography>
-              <Typography variant="body2" gutterBottom>
-                Full resolution 1920x1080 • JPEG
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                ID: 1030114
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                Remove
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" component="div">
-              $19.00
+    <Grid item xs={12} md={6}>
+      <CardActionArea component="a" href="#">
+        <Card sx={{ display: 'flex' }}>
+          <CardContent sx={{ flex: 1 }}>
+            <Typography component="h2" variant="h5">
+              title
             </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Paper>
-  ); 
-};
-
-export default function NestedGrid() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1}>
-        <Grid container item spacing={3}>
-          <ComplexGrid />
-        </Grid>
-        <Grid container item spacing={3}>
-          <ComplexGrid />
-        </Grid>
-        <Grid container item spacing={3}>
-          <ComplexGrid />
-        </Grid>
-      </Grid>
-    </Box>
+            <Typography variant="subtitle1" color="text.secondary">
+              4/20
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              desc
+            </Typography>
+            <Typography variant="subtitle1" color="primary">
+              Continue reading...
+            </Typography>
+          </CardContent>
+          <CardMedia
+            // component="img"
+            // sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+            // image={post.image}
+            // alt={post.imageLabel}
+          />
+        </Card>
+      </CardActionArea>
+    </Grid>
   );
 }
+
+FeaturedPost.propTypes = {
+  post: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    imageLabel: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default FeaturedPost;
